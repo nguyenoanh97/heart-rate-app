be2cb45ac8a082b2df723eb971b9e0578136926c
Oanh <oanh0444@gmail.com>
2019-11-14 23:39:03 +0700

    fix button

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          login init project
         </a>
       </header>
     </div>
